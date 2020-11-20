e00180726cc366b561665438c05357bd762c7545
Lucie Bakels <lucie_bakels@hotmail.com>
2020-11-20 15:01:50 +0800

    added snapshot.py documentations

--- source/snapshot/snapshot.py
+++ source/snapshot/snapshot.py
@@ -14,16 +14,16 @@ class Unbuffered(object):
 	Copied from stackoverflow
 	This forces HPC facilities to print when print is called
 	"""
-   def __init__(self, stream):
-       self.stream = stream
-   def write(self, data):
-       self.stream.write(data)
-       self.stream.flush()
-   def writelines(self, datas):
-       self.stream.writelines(datas)
-       self.stream.flush()
-   def __getattr__(self, attr):
-       return getattr(self.stream, attr)
+	def __init__(self, stream):
+		self.stream = stream
+	def write(self, data):
+		self.stream.write(data)
+		self.stream.flush()
+	def writelines(self, datas):
+		self.stream.writelines(datas)
+		self.stream.flush()
+	def __getattr__(self, attr):
+		return getattr(self.stream, attr)
 
 sys.stdout = Unbuffered(sys.stdout)
 
